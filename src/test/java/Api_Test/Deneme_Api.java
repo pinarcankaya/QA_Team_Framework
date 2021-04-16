ef7dfd2d00251ecd041223eeee08f2979891e6c2
pinarcankaya <pinarunalcan50@gmail.com>
2021-04-16 17:50:17 +0200

    ApiGoRest first commit

--- src/test/java/Api_Test/Deneme_Api.java
+++ src/test/java/Api_Test/Deneme_Api.java
@@ -3,9 +3,13 @@ package Api_Test;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 import static io.restassured.RestAssured.given;
 
@@ -17,13 +21,58 @@ public class Deneme_Api {
     JsonPath json;
 
     @Test
-    public void TC01() throws IOException {
+    public void setup() {
         response = given().
                 accept(ContentType.JSON).
                 when().
                 get(endPoint);
         json = response.jsonPath();
         response.prettyPrint();
-
     }
+        @Test
+        public void TC01() {
+            setup();
+
+            int totalData = json.getInt("meta.pagination.total");
+            System.out.println(totalData);
+            Assert.assertNotEquals(totalData, 1375);
+            int pages = json.getInt("meta.pagination.pages");
+            System.out.println(pages);
+            Assert.assertNotEquals(pages, 20);
+
+        }
+
+        @Test
+        public void TC02() {
+            setup();
+            List<Integer> idListOrder = json.getList("data.id");
+            System.out.println(idListOrder);
+
+            for (int i = 0; i < idListOrder.size(); i++) {
+                for (int j = i + 1; j < idListOrder.size(); j++) {
+                    // System.out.println(list.get(i) + "----" +list.get(j));
+                    Assert.assertTrue(idListOrder.get(i) < idListOrder.get(j));
+                }
+            }
+        }
+
+        @Test  //id unique assertion
+        public void TC03() {
+            setup();
+            List<Integer> idList = json.getList("data.id");
+            for (int i = 0; i < idList.size(); i++) {
+                for (int j = i + 1; j < idList.size(); j++) {
+
+                    Assert.assertTrue(idList.get(i) != idList.get(j));
+                }
+            }
+            //2.yol
+            Set<Integer> linkedHashSet = new LinkedHashSet<Integer>(idList);
+            //  System.out.println(idList.size());
+            // System.out.println(linkedHashSet.size());
+            Assert.assertEquals(idList.size(),linkedHashSet.size());
+
+        }
+
+
 }
