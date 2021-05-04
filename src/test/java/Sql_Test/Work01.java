28a53f8db1a86b69ce5ffd2635b08323c48f4434
zuhreatay <zuhreatay1@gmail.com>
2021-05-04 11:47:45 -0500

    Sql zuhre branchden

--- src/test/java/Sql_Test/Work01.java
+++ src/test/java/Sql_Test/Work01.java
@@ -4,11 +4,10 @@ import Utilities.DatabaseConnector;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Work01 {
 
@@ -24,21 +23,28 @@ public class Work01 {
         list1=DatabaseConnector.getQueryAsAListOfMaps(query1);
         System.out.println(list1.get(0).get("count(title)"));
 
-      // Assert.assertTrue(list1.get(0).containsValue("63"), String.valueOf(63));
-       Assert.assertEquals(list1.get(0).get("count(title)"),"63");
+      Assert.assertEquals(list1.get(0).get("count(title)"),"63");
 
 
     }
 
     @Test//Suresi 90 ile 120 dakika arasinda olan filmleri kisadan uzuna dogru siralayiniz.90 dk olan 5 film oldugunu assert ediniz
     public void soru02() {
-        String query2 = "select *\n" +
+        String query2 = "select length\n" +
                 "from sakila.film\n" +
                 "where length between '90' and '120'\n" +
                 "order by length asc;";
         list1=DatabaseConnector.getQueryAsAListOfMaps(query2);
         System.out.println(list1);
-        Assert.assertTrue(list1.get(0).containsValue("90"), String.valueOf(5));
+
+        int count=0;
+        for ( Map<String, String> w : list1) {
+            if (w.get("length").equals("90"))
+                count++;
+
+        }
+        System.out.println(count);
+       Assert.assertEquals(count ,5);
 
     }
 
@@ -47,8 +53,8 @@ public class Work01 {
         String query3 = "select max(length) as enuzunfilm,title\n" +
                 "from sakila.film";
         list1=DatabaseConnector.getQueryAsAListOfMaps(query3);
-        System.out.println(list1);
-        Assert.assertTrue(list1.get(0).containsValue("185"), String.valueOf(185));
+        System.out.println(list1.get(0).get("enuzunfilm"));
+        Assert.assertEquals(list1.get(0).get("enuzunfilm"),"185");
 
 
     }
@@ -59,7 +65,7 @@ public class Work01 {
                 "from sakila.film;";
         list1=DatabaseConnector.getQueryAsAListOfMaps(query4);
         System.out.println(list1.size());
-        Assert.assertTrue(list1.get(0).containsValue("115.272"), String.valueOf(115.272));//Test gecmedi Expected true but Actual false
+         Assert.assertEquals(list1.get(0).get("avg(length)"),"115.2720");
 
     }
 
@@ -67,14 +73,19 @@ public class Work01 {
     public void soru05() {
 
         String query5="\n" +
-                "select title,length\n" +
+                "select length\n" +
                 "from sakila.film\n" +
                 "where length < (select max(length)  \n" +
                 "                    from sakila.film );";
         list1=DatabaseConnector.getQueryAsAListOfMaps(query5);
-        int maxSize=list1.size();
-        Assert.assertEquals(list1.get(0).size(), maxSize);//gecmedi Actual ==>2 expected ==>990
-    }
+        System.out.println(list1);
+      //  int maxSize=list1.size();
 
+
+        for (Map<String, String> w: list1) {
+
+        }
+
+    }
 
 }
